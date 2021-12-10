6dc39fb5276f9e9e3334c21d82d349e2a5d698ec
Arsenii Yurchenko <arsenii.yurchenko@nixsolutions.com>
2021-12-10 17:17:03 +0200

    Add food page header component

---
+++ src/components/FoodPage/Header/Header.js
@@ -0,0 +1,24 @@
+import { createElement } from '../../../libs/DOM';
+import './Header.scss';
+
+const Header = () => {
+  const openModal = () => {
+    document.getElementById('add-recipe-modal-window').style.display = 'block';
+  };
+
+  return createElement('div', { class: 'food-page-header' }, [
+    createElement('h1', { class: 'food-page-header-title' }, 'food'),
+    createElement(
+      'button',
+      {
+        class: 'food-page-header-button',
+        onclick: () => {
+          openModal();
+        },
+      },
+      'Add new recipe'
+    ),
+  ]);
+};
+
+export default Header;
