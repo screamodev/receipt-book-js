f59c4eceaca1c005bc432dd2ab171fb261d73d45
Arsenii Yurchenko <arsenii.yurchenko@nixsolutions.com>
2021-12-10 17:37:47 +0200

    Mount header and modal window

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import './assets/scss/styles.scss';
 import { fetchRecipes } from './api/recipesApi';
 import RecipeCards from './components/FoodPage/RecipeCards';
 import { mount } from './libs/DOM';
+import Header from './components/FoodPage/Header/Header';
+import AddRecipeModal from './components/FoodPage/Header/AddRecipeModal';
+
+mount(document.getElementById('add-recipes-holder'), Header());
+
+mount(document.getElementById('main'), AddRecipeModal());
 
 fetchRecipes().then((recipes) =>
   mount(document.getElementById('explore-recipes'), RecipeCards({ recipes }))
