070fec7023838bd40b913129987017c64e1525c9
Arsenii Yurchenko <arsenii.iurchenko@gmail.com>
2021-12-15 12:21:13 +0200

    Add block structure to constants

--- src/constants/elementSelectors.js
+++ src/constants/elementSelectors.js
@@ -1,7 +1,9 @@
 export const CREATE_MAIN_ID = 'main';
 export const CREATE_HEADER_HOLDER_ID = 'header-holder';
+
 export const CREATE_RECIPES_ID = 'explore-recipes';
 export const CREATE_RECIPES_CARDS_ID = 'recipes-cards';
+
 export const CREATE_RECIPE_MODAL_ID = 'add-recipe-modal-window';
 export const CREATE_RECIPE_FORM_INGREDIENT_LIST_ID =
   'add-recipe-form-ingredients-list';
