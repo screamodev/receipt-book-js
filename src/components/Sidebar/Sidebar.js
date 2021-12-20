4b179db12a2a42f812905b93fb8a3f7b8c257d0a
Arsenii Yurchenko <arsenii.iurchenko@gmail.com>
2021-12-20 12:53:38 +0200

    Add sidebar items href. Move onclick event to the sidebar component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -7,27 +7,37 @@ import bookmarkIcon from '../../assets/icons/bookmark-icon.svg';
 import SidebarElement from './SidebarElement';
 
 const Sidebar = () => {
+  let sidebarStatus = false;
   const sidebarItems = [
     {
       name: 'Menu',
       icon: menuIcon,
       iconAlt: 'menu-icon',
+      href: '/',
     },
     {
       name: 'Food',
       icon: foodIcon,
       iconAlt: 'food-icon',
+      href: '/food',
     },
     {
       name: 'Bookmarks',
       icon: bookmarkIcon,
       iconAlt: 'bookmark-icon',
+      href: '/bookmarks',
     },
   ];
 
+  const showSidebar = () => {
+    const sidebar = document.getElementById(SIDEBAR);
+    sidebarStatus = !sidebarStatus;
+    sidebar.style.transform = `translateX(${sidebarStatus ? 0 : -50}%)`;
+  };
+
   return createElement(
     'aside',
-    { id: SIDEBAR },
+    { id: SIDEBAR, onclick: showSidebar },
     createElement(
       'nav',
       { class: 'nav' },
