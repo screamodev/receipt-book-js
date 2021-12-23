377bed2f96ee2871a6a56f1152c062b210108151
Arsenii Yurchenko <arsenii.iurchenko@gmail.com>
2021-12-23 14:57:00 +0200

    Delete bookmark icon, change some recipe info styles

--- src/components/RecipeInfo/RecipeInfo.js
+++ src/components/RecipeInfo/RecipeInfo.js
@@ -1,6 +1,5 @@
 import { createElement } from '../../libs/DOM';
 import './recipeInfo.scss';
-import bookmarkIcon from '../../assets/icons/bookmark-white-icon.png';
 import ingredientIcon from '../../assets/icons/Food-Cutlery-icon.png';
 
 const RecipeInfo = ({ name, description, ingredients, imgUrl }) =>
@@ -10,95 +9,84 @@ const RecipeInfo = ({ name, description, ingredients, imgUrl }) =>
       id: 'recipe-info-wrapper',
       style: `background: url(${imgUrl}) #F7E4BB no-repeat top; background-size: 423px 318px;`,
     },
-    [
-      createElement(
-        'button',
-        { class: 'recipe-bookmark-icon' },
-        createElement('img', {
-          class: 'recipe-bookmark-icon-img',
-          src: bookmarkIcon,
-          alt: 'bookmark-icon',
-        })
-      ),
-      createElement(
-        'div',
-        {
-          class: 'recipe-info',
-        },
-        [
-          createElement(
-            'h2',
-            {
-              class: 'recipe-info-name',
-            },
-            name
-          ),
-          createElement(
-            'div',
-            {
-              class: 'recipe-description',
-            },
-            description
-          ),
-          createElement(
-            'div',
-            {
-              class: 'recipe-ingredients-holder',
-            },
-            [
-              createElement(
-                'h3',
-                {
-                  class: 'recipe-ingredients-title',
-                },
-                'Ingredients'
-              ),
-              createElement(
-                'div',
-                {
-                  class: 'recipe-ingredients',
-                },
-                ingredients.map(
-                  ({ name: ingredientName, weight: ingredientWeight }) =>
-                    createElement(
-                      'div',
-                      {
-                        class: 'ingredient-item',
-                      },
-                      [
-                        createElement(
-                          'div',
-                          {
-                            class: 'ingredient-item-image',
-                          },
-                          createElement('img', {
-                            src: ingredientIcon,
-                            alt: 'dish',
-                          })
-                        ),
-                        createElement(
-                          'span',
-                          {
-                            class: 'ingredient-item-name',
-                          },
-                          ingredientName
-                        ),
-                        createElement(
-                          'span',
-                          {
-                            class: 'ingredient-item-pcs',
-                          },
-                          ingredientWeight
-                        ),
-                      ]
-                    )
-                )
-              ),
-            ]
-          ),
-        ]
-      ),
-    ]
+    createElement(
+      'div',
+      {
+        class: 'recipe-info',
+      },
+      [
+        createElement(
+          'h2',
+          {
+            class: 'recipe-info-name',
+          },
+          name
+        ),
+        createElement(
+          'div',
+          {
+            class: 'recipe-description',
+          },
+          description
+        ),
+        createElement(
+          'div',
+          {
+            class: 'recipe-ingredients-holder',
+          },
+          [
+            createElement(
+              'h3',
+              {
+                class: 'recipe-ingredients-title',
+              },
+              'Ingredients'
+            ),
+            createElement(
+              'div',
+              {
+                class: 'recipe-ingredients',
+              },
+              ingredients.map(
+                ({ name: ingredientName, weight: ingredientWeight }) =>
+                  createElement(
+                    'div',
+                    {
+                      class: 'ingredient-item',
+                    },
+                    [
+                      createElement(
+                        'div',
+                        {
+                          class: 'ingredient-item-image',
+                        },
+                        createElement('img', {
+                          src: ingredientIcon,
+                          alt: 'dish',
+                        })
+                      ),
+                      createElement(
+                        'span',
+                        {
+                          class: 'ingredient-item-name',
+                        },
+                        ingredientName
+                      ),
+                      createElement(
+                        'span',
+                        {
+                          class: 'ingredient-item-pcs',
+                        },
+                        ingredientWeight
+                      ),
+                    ]
+                  )
+              )
+            ),
+          ]
+        ),
+      ]
+    )
   );
 
 export default RecipeInfo;
